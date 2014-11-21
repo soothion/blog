5ca7cb0cb2177cac83b672b0b2631edd62c7d5b3
soothion <soothion@sina.com>
2014-11-21 23:02:32 +0800

    update nav.php

--- protected/components/views/nav.php
+++ protected/components/views/nav.php
@@ -1,4 +1,4 @@
-<?php $alias = $_REQUEST['alias']?:'';?>
+<?php @$alias = $_REQUEST['alias']?:'';?>
 <ul>
     <li <?php if(Yii::app()->controller->id=='index') echo 'class="current-item"';?>><a href="<?php echo Yii::app()->createUrl('index');?>">首页</a></li>
     <li <?php if(Yii::app()->controller->id=='blog'&&$alias!='demo'&&$alias!='about'&&$alias!='service'&&$alias!='contact') echo 'class="current-item"';?>><a href="<?php echo Yii::app()->createUrl('blog');?>">博客</a></li>
